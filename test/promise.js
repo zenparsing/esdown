dbe6d5e06c445fa947265b5312b329ff1a4d6179
khs4473 <zenparsing@gmail.com>
2013-12-20 14:32:42 -0500

    Experimentally get rid of thenable coercion.  Yeah, it works.

--- test/promise.js
+++ test/promise.js
@@ -1,13 +1,46 @@
 import {} from "../runtime/Promise.js";
 
+class BorgPromise extends Promise {
+
+    then(onResolve, onReject) {
+
+        if (typeof onResolve !== "function") onResolve = x => x;
+
+        return super.then(x => {
+
+            // NOTE: x is a non-promise
+            return (x && typeof x.then === "function") ?
+                x.then(onResolve, onReject) :
+                onResolve(x);
+            
+        }, onReject);
+    }
+}
+
 var p = Promise.resolve(0);
 var q = Promise.resolve(p);
-var r = Promise.resolve(q);
+var r = BorgPromise.resolve(q);
+
+var thenable = {
+
+    then(onResolve, onReject) {
+    
+        console.log("thenable called.");
+        
+        Promise.resolve().then($=> {
+            onResolve("thenable!");
+        });
+    }
+};
+
+r.then(x => thenable).then(x => console.log(x), err => console.log(err));
 
 /*p.then(x => r).chain(console.log);*/
 
+/*
 Promise.__iterate(function*() {
 
     return p;
     
-}()).chain(console.log, x => setTimeout($=> { throw x }, 0));
+}()).then(x => console.log(x), x => setTimeout($=> { throw x }, 0));
+*/
