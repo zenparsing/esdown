7ba848e88b73e87e0777c868c677cbd7406bb377
zenparsing <zenparsing@gmail.com>
2014-06-16 00:44:32 -0400

    Add a simple test for Map shim.

--- test/shim/mapset.js
+++ test/shim/mapset.js
@@ -1,3 +1,32 @@
+var data = [
+
+    ["foo", "bar"],
+    ["constructor", "zootsuit"],
+    [1, {}],
+    [NaN, "hello"],
+    [Infinity, "world"]
+    
+];
+
+
 export var tests = {
     
+    "construct" (test) {
+    
+        var map = new Map;
+        
+        data.forEach(([key, val]) => map.set(key, val));
+        
+        var i = 0;
+        
+        map.forEach((val, key) => { 
+        
+            test
+            .equals(key, data[i][0])
+            .equals(val, data[i][1])
+            ;
+            
+            ++i;
+        });
+    }
 };
