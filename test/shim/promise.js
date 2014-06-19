674823287c2095e20c98daf247d7c3fe9ddc67ba
zenparsing <zenparsing@gmail.com>
2014-06-19 00:52:31 -0400

    Add more tests for Promise.race.

--- test/shim/promise.js
+++ test/shim/promise.js
@@ -36,8 +36,36 @@ export var tests = {
         "rejects if all promises are settled and the ordinally-first is rejected" (test) {
         
             var list = [Promise.reject(1), Promise.reject(2), Promise.resolve(3)];
-
             return either(Promise.race(list)).then(x => test.equals(x, 1));
+        },
+        
+        "settles in the same way as the first promise to settle" (test) {
+        
+            var p2 = delayed(2, 200);
+            var p1 = delayed(1, 1000);
+            var p3 = delayed(3, 500);
+            var list = [p1, p2, p3];
+
+            return either(Promise.race(list)).then(x => test.equals(x, 2));
+        },
+        
+        "never settles when given an empty iterable" (test) {
+        
+            var iterable = [];
+            var settled = false;
+
+            either(Promise.race(iterable)).then($=> settled = true);
+            return delayed(null, 300).then($=> test.assert(!settled));
+        },
+        
+        "rejects with a TypeError if given a non-iterable" (test) {
+        
+            var notIterable = {};
+
+            return Promise.race(notIterable).then(
+                $=> test.assert(false),
+                reason => test.assert(reason instanceof TypeError)
+            );
         },
         
     }
