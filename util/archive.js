d45f0d365051a77fdcf51927039144fb5e4f7e74
zenparsing <zenparsing@gmail.com>
2014-11-05 13:45:44 -0500

    Create the archive directory if it doesn't exist.

--- util/archive.js
+++ util/archive.js
@@ -1,7 +1,7 @@
 /*
 
 Archive the current esdown.js build file to the "_prev" folder.  Always run this before
-building, since a bad build will clobber esdown.  If you create a bad build, you can 
+building, since a bad build will clobber esdown.  If you create a bad build, you can
 restore esdown.js using "restore.js".
 
 */
@@ -15,7 +15,7 @@ function copy() {
 
     var source = Path.resolve(__dirname, "../build/esdown.js"),
         target = targetName();
-    
+
     console.log("Archiving to [" + target + "]...");
     FS.writeFileSync(targetName(), FS.readFileSync(source, { encoding: "utf8" }));
     console.log("Done.");
@@ -23,27 +23,32 @@ function copy() {
 
 function targetName() {
 
-    var files = FS.readdirSync(Path.resolve(__dirname, "../_prev")),
+    var dir = Path.resolve(__dirname, "../_archive");
+
+    if (!FS.existsSync(dir))
+        FS.mkdirSync(dir);
+
+    var files = FS.readdirSync(dir),
         now = new Date,
         name = "esdown-",
         num = 0;
-    
+
     name += [now.getFullYear(), pad2(now.getMonth() + 1), pad2(now.getDate())].join("");
-    
+
     while (files.indexOf(name + "-" + pad2(num) + ".js") !== -1)
         num++;
-    
+
     name += "-" + pad2(num) + ".js";
-    
-    return Path.resolve(__dirname, "../_prev/", name);
+
+    return Path.resolve(dir, name);
 }
 
 function pad2(n) {
 
     n = String(n);
-    
+
     while (n.length < 2)
         n = "0" + n;
-    
+
     return n;
 }
--- util/restore.js
+++ util/restore.js
@@ -13,7 +13,7 @@ function restore() {
 
     var target = Path.resolve(__dirname, "../build/esdown.js"),
         source = sourceName();
-    
+
     console.log("Restoring from [" + source + "]...");
     FS.writeFileSync(target, FS.readFileSync(source, { encoding: "utf8" }));
     console.log("Done.");
@@ -21,13 +21,19 @@ function restore() {
 
 function sourceName() {
 
-    var files = FS.readdirSync(Path.resolve(__dirname, "../_prev"));
-    
+    var dir = Path.resolve(__dirname, "../_archive");
+
+    // Create the archive directory if it doesn't already exist
+    if (!FS.existsSync(dir))
+        FS.mkdirSync(dir);
+
+    var files = FS.readdirSync(dir);
+
     files = files.filter(function(name) { return name.indexOf("esdown-") === 0; });
     files = files.sort();
-    
+
     if (files.length === 0)
         throw new Error("No archived files were found.");
-    
-    return Path.resolve(__dirname, "../_prev/", files[files.length - 1]);
+
+    return Path.resolve(dir, files[files.length - 1]);
 }
