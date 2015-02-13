e90224e77f66319eba55b3469d92a2da03b47f0f
zenparsing <zenparsing@gmail.com>
2015-02-12 23:53:11 -0500

    Add basic block scoping translation tests.

---
+++ test/translate/input/block-scope.js
@@ -0,0 +1,23 @@
+let x;
+const y = 0;
+
+{
+    let x;
+}
+
+for (let i = 0; i < 10; ++i) {}
+
+{
+    let z;
+}
+
+{
+    let z;
+}
+
+{
+    let a;
+    {
+        let a;
+    }
+}
---
+++ test/translate/input/block-scope.out.js
@@ -0,0 +1,23 @@
+var x;
+var y = 0;
+
+{
+    var x$0;
+}
+
+for (var i$0 = 0; i$0 < 10; ++i$0) {}
+
+{
+    var z$0;
+}
+
+{
+    var z$1;
+}
+
+{
+    var a$0;
+    {
+        var a$1;
+    }
+}
