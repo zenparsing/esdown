a84d90ebb67be1b574fdf9000614af37dc256b08
zenparsing <zenparsing@gmail.com>
2015-12-11 00:16:33 -0500

    Bump version.

--- esdown-runtime/default.js
+++ esdown-runtime/default.js
@@ -1,4 +1,4 @@
-const VERSION = "1.0.4";
+const VERSION = "1.0.5";
 
 const GLOBAL = (function() {
 
--- src/Runtime.js
+++ src/Runtime.js
@@ -2,7 +2,7 @@ export let Runtime = {};
 
 Runtime.API = 
 
-`var VERSION = "1.0.4";
+`var VERSION = "1.0.5";
 
 var GLOBAL = (function() {
 
