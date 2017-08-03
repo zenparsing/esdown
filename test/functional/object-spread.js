4c9cb691ef0f08207fba5ab051e4b2315ecd5795
Kevin Smith <KevinH.Smith@coxinc.com>
2017-08-03 11:12:36 -0400

    Add object spread test

--- test/functional/object-spread.js
+++ test/functional/object-spread.js
@@ -6,4 +6,9 @@ export const tests = {
       { a: 1, b: 2, c: 3, d: 4, e: 5 });
   },
 
+  'spreading null and undefined' (test) {
+    test.equals({ a: 1, ...null, b: 2 }, { a: 1, b: 2 });
+    test.equals({ a: 1, ...undefined, b: 2 }, { a: 1, b: 2 });
+  },
+
 };
